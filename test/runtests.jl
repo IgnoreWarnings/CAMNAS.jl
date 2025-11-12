4f5db4619c31e70aca5837f6535ce9e9b665e789
Pascal Henry Bauer <pascal.bauer@rwth-aachen.de>
2025-11-12 22:36:00 +0100

    wip

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,88 +18,128 @@ using Suppressor
         parse(Int32, sys_mat_str[4]),
         parse(Int32, sys_mat_str[5])
     )
-    sys_mat_ptr = pointer_from_objref(sys_mat)
+    # sys_mat_ptr = pointer_from_objref(sys_mat)
 
-    rhs_vec_strings = readlines("rhs_small.txt")
-    rhs_vec_strings = replace.(rhs_vec_strings, r"[\[\],]" => "")
-    rhs_vec = parse.(Float64, split(rhs_vec_strings[1]))
+    # rhs_vec_strings = readlines("rhs_small.txt")
+    # rhs_vec_strings = replace.(rhs_vec_strings, r"[\[\],]" => "")
+    # rhs_vec = parse.(Float64, split(rhs_vec_strings[1]))
 
-    lhs_vec = zeros(Float64, length(rhs_vec))
+    # lhs_vec = zeros(Float64, length(rhs_vec))
 
-    @testset "Initialization" begin
-        @test init(Base.unsafe_convert(Ptr{dpsim_csr_matrix}, sys_mat_ptr)) == 0
-    end 
+    # @testset "Initialization" begin
+    #     @test init(Base.unsafe_convert(Ptr{dpsim_csr_matrix}, sys_mat_ptr)) == 0
+    # end 
 
-    @testset "Solving" begin
-        @test solve(Base.unsafe_convert(Ptr{Cdouble}, rhs_vec), Base.unsafe_convert(Ptr{Cdouble}, lhs_vec)) == 0
-        @test lhs_vec == [1.0, 1/2, 1/3]
-    end
+    # @testset "Solving" begin
+    #     @test solve(Base.unsafe_convert(Ptr{Cdouble}, rhs_vec), Base.unsafe_convert(Ptr{Cdouble}, lhs_vec)) == 0
+    #     @test lhs_vec == [1.0, 1/2, 1/3]
+    # end
 
-    @testset "Decomposition" begin
-        # Change system matrix values
-        sys_mat_bak = sys_mat.values
-        sys_mat.values = Base.unsafe_convert(Ptr{Cdouble}, [4.0, 5.0, 6.0])
+    # @testset "Decomposition" begin
+    #     # Change system matrix values
+    #     sys_mat_bak = sys_mat.values
+    #     sys_mat.values = Base.unsafe_convert(Ptr{Cdouble}, [4.0, 5.0, 6.0])
 
-        # Decompose new system matrix
-        @test decomp(Base.unsafe_convert(Ptr{dpsim_csr_matrix}, sys_mat_ptr)) == 0
+    #     # Decompose new system matrix
+    #     @test decomp(Base.unsafe_convert(Ptr{dpsim_csr_matrix}, sys_mat_ptr)) == 0
 
-        # Solve system with new matrix
-        @test solve(Base.unsafe_convert(Ptr{Cdouble}, rhs_vec), Base.unsafe_convert(Ptr{Cdouble}, lhs_vec)) == 0
-        @test lhs_vec == [1/4, 1/5, 1/6]
+    #     # Solve system with new matrix
+    #     @test solve(Base.unsafe_convert(Ptr{Cdouble}, rhs_vec), Base.unsafe_convert(Ptr{Cdouble}, lhs_vec)) == 0
+    #     @test lhs_vec == [1/4, 1/5, 1/6]
 
-        # Restore system matrix values
-        sys_mat.values = sys_mat_bak
-    end        
+    #     # Restore system matrix values
+    #     sys_mat.values = sys_mat_bak
+    # end        
 
-    @testset "Generator" begin
+    # @testset "Generator" begin
+    #     include("Generator.jl")
+    #     include("MatrixValidator.jl")
+
+    #     settings = Generator.Settings(
+    #         dimension=3000,
+    #         density=0.1,
+    #         magnitude_off=0.05,
+    #         delta=0.5,
+    #         seed=1337
+    #     )
+
+    #     matrix = Generator.generate_matrix(settings)
+
+    #     # Dimensions
+    #     @test MatrixValidator.is_quadratic(matrix)
+    #     @test MatrixValidator.m(matrix) == settings.dimension
+    #     @test MatrixValidator.n(matrix) == settings.dimension
+
+    #     # Density
+    #     density = MatrixValidator.density(matrix)
+    #     @test isapprox(density, settings.density; atol=0.001)
+
+    #     # Condition
+    #     condition_tresh = 2
+    #     @test MatrixValidator.condition(matrix) < condition_tresh
+
+    #     # LU-decomposability
+    #     @test MatrixValidator.is_lu_decomposable(matrix)
+
+    #     # Solving and rhs generation
+    #     rhs = Generator.generate_rhs_vector(matrix; prefered_solution=ones(Float64, size(matrix, 1)))
+    #     x = matrix \ rhs
+    #     tolerance = 1e-8
+    #     @test all(value -> isapprox(value, 1.0; atol=tolerance), x)
+
+    #     # Random seed
+    #     @test matrix == Generator.generate_matrix(settings) # Reproducability
+
+    #     new_settings = Generator.Settings(
+    #         dimension=settings.dimension,
+    #         density=settings.density,
+    #         magnitude_off=settings.magnitude_off,
+    #         delta=settings.delta,
+    #         seed=settings.seed + 1
+    #     )
+
+    #     @test matrix != Generator.generate_matrix(new_settings) # Randomness
+
+    # end
+
+    @testset "Benchmark" begin
+        include("Benchmark.jl")
         include("Generator.jl")
-        include("MatrixValidator.jl")
+
+        cpu = Benchmark.benchmark("/home/bauer/Codebase/CAMNAS.jl/benchmark/system_matrix_(2000)_(0.1).txt",
+            "/home/bauer/Codebase/CAMNAS.jl/benchmark/rhs_(2000)_(0.1).txt",
+            CAMNAS.NoAccelerator())
+        print(cpu)
 
         settings = Generator.Settings(
-            dimension=3000,
-            density=0.1,
-            magnitude_off=0.05,
-            delta=0.5,
+            dimension=4000,
+            density=0.01,
             seed=1337
         )
 
         matrix = Generator.generate_matrix(settings)
+        rhs_vector = Generator.generate_rhs_vector(matrix)
 
-        # Dimensions
-        @test MatrixValidator.is_quadratic(matrix)
-        @test MatrixValidator.m(matrix) == settings.dimension
-        @test MatrixValidator.n(matrix) == settings.dimension
+        accelerators = [CAMNAS.NoAccelerator(), CAMNAS.CUDAccelerator()]
+        for accelerator in accelerators
+            Benchmark.benchmark(matrix, rhs_vector, accelerator)
+        end
 
-        # Density
-        density = MatrixValidator.density(matrix)
-        @test isapprox(density, settings.density; atol=0.001)
-
-        # Condition
-        condition_tresh = 2
-        @test MatrixValidator.condition(matrix) < condition_tresh
-
-        # LU-decomposability
-        @test MatrixValidator.is_lu_decomposable(matrix)
-
-        # Solving and rhs generation
-        rhs = Generator.generate_rhs_vector(matrix; prefered_solution=ones(Float64, size(matrix, 1)))
-        x = matrix \ rhs
-        tolerance = 1e-8
-        @test all(value -> isapprox(value, 1.0; atol=tolerance), x)
-
-        # Random seed
-        @test matrix == Generator.generate_matrix(settings) # Reproducability
-
-        new_settings = Generator.Settings(
-            dimension=settings.dimension,
-            density=settings.density,
-            magnitude_off=settings.magnitude_off,
-            delta=settings.delta,
-            seed=settings.seed + 1
-        )
-
-        @test matrix != Generator.generate_matrix(new_settings) # Randomness
-
+        # for dimension in dimensions
+        #     for density in densities
+        #         # Generate Test Matrixes and RHS vectors
+        #         print("Generating...")
+        #         matrix = Generator.generate_matrix(dimension; density=density)
+        #         csr_matrix = Generator.to_csr(matrix)
+        #         rhs_vector = Generator.generate_rhs_vector(matrix)
+        #         matrix_path = "$benchmarkPath/system_matrix_($dimension)_($density).txt"
+        #         rhs_path = "$benchmarkPath/rhs_($dimension)_($density).txt"
+        #         Generator.to_files(csr_matrix, rhs_vector;
+        #             matrix_path=matrix_path,
+        #             rhs_path=rhs_path)
+        #         println("Done.")
+        #     end
+        # end
     end
 end # testset "CAMNAS"
 
--- test/test_interface.jl
+++ test/test_interface.jl
@@ -20,40 +20,8 @@ begin # Initialization
     using CAMNAS
     using Profile
 
-    struct ArrayPath path::String end
-    struct VectorPath path::String end
-
-    function read_input(path::ArrayPath)
-        # Read system matrix from file
-        system_matrix_strings = readlines(path.path)
-
-        # Sanize strings
-        system_matrix_strings = replace.(system_matrix_strings, r"[\[\],]" => "")
-
-        # Convert system to dpsim_csr_matrix
-        values = parse.(Float64, split(system_matrix_strings[1]))
-        rowIndex = parse.(Cint, split(system_matrix_strings[2]))
-        colIndex = parse.(Cint, split(system_matrix_strings[3]))
-
-        system_matrix = CAMNAS.dpsim_csr_matrix(
-            Base.unsafe_convert(Ptr{Cdouble}, values),
-            Base.unsafe_convert(Ptr{Cint}, rowIndex),
-            Base.unsafe_convert(Ptr{Cint}, colIndex),
-            parse(Int32, system_matrix_strings[4]),
-            parse(Int32, system_matrix_strings[5])
-        )
-
-        return system_matrix
-    end
-
-    function read_input(path::VectorPath)
-        # Reard right hand side vector from file
-        rhs_vector_strings = readlines(path.path)
-
-        # Sanitize rhs strings and parse into Float64 vector
-        rhs_vector_strings = replace.(rhs_vector_strings, r"[\[\],]" => "")
-        rhs_vector = parse.(Float64, split(rhs_vector_strings[1]))
-    end
+    include("Utils.jl")
+    using .Utils
 
     if inputValues == "generated"
         include("Generator.jl")
@@ -65,8 +33,8 @@ begin # Initialization
         Generator.to_files(csr_matrix, rhs_vector)
     end
 
-    system_matrix = read_input(ArrayPath("$(@__DIR__)/system_matrix_$inputValues.txt"))
-    rhs_vector = read_input(VectorPath("$(@__DIR__)/rhs_$inputValues.txt"))
+    system_matrix = Utils.read_input(ArrayPath("$(@__DIR__)/system_matrix_$inputValues.txt"))
+    rhs_vector = Utils.read_input(VectorPath("$(@__DIR__)/rhs_$inputValues.txt"))
 
     GC.enable(false) # We cannot be sure that system_matrix is garbage collected before the pointer is passed...
 
@@ -82,7 +50,7 @@ end # end Initialization
 
 begin # Decomposition step
     GC.enable(false)
-    system_matrix = read_input(ArrayPath("$(@__DIR__)/system_matrix_$inputValues.txt"))
+    system_matrix = Utils.read_input(ArrayPath("$(@__DIR__)/system_matrix_$inputValues.txt"))
     system_matrix_ptr = pointer_from_objref(system_matrix)
     @time decomp(Base.unsafe_convert(Ptr{dpsim_csr_matrix}, system_matrix_ptr))
     GC.enable(true)
