15a0b3b6fda9f2b19c8279218da8df08103c35b9
Pascal Henry Bauer <pascal.bauer@rwth-aachen.de>
2026-06-17 14:13:37 +0200

    refactor(Benchmark): Refactor Benchmark to get configured by Camnas
    Signed-off-by: Pascal Henry Bauer <pascal.bauer@rwth-aachen.de>

--- test/Benchmark.jl
+++ test/Benchmark.jl
@@ -9,101 +9,46 @@ using CAMNAS
 
 using Base
 using BenchmarkTools
-using SparseMatricesCSR
+using CSV, DataFrames
 
-include("Utils.jl")
-using .Utils
+function get_next_folder(base_dir="benchmark/")
+        runs = filter(name -> occursin(r"^run_\d+$", name), readdir(base_dir))
 
-@kwdef struct BenchmarkResult
-    decomp_elapses::Float64
-    solve_elapses::Float64
+        if isempty(runs)
+            return joinpath(base_dir, "run_1")
+        end
+
+        nums = parse.(Int, replace.(runs, r"^run_" => ""))
+        next_num = maximum(nums) + 1
+
+        return joinpath(base_dir, "run_$(next_num)")
+    end
+
+function benchmark_solve(rhs::Vector{Float64})
+    solve = @elapsed begin
+        lhs = zeros(Float64, length(rhs))
+        CAMNAS.solve(Base.unsafe_convert(Ptr{Cdouble}, rhs), Base.unsafe_convert(Ptr{Cdouble}, lhs))
+    end
+
+    Dict("solve" => solve)
 end
 
 """
-    function benchmark(dpsim_matrix::dpsim_csr_matrix, rhs_vector::Vector{Float64}; samples::UInt=UInt(3))
+    function save_csv(path::AbstractString, benchmark_metrics::Benchmarkmetrics, matrix_path::String)
 
-*Note: Camnas debug output gets disabled during this funcion for performance reasons*
-
-This function benchmarks the currently active system configutaion by measuring lu-decomposing and solving times 
-    of a given `dpsim_matrix::dpsim_csr_matrix` with a `rhs_vector::Vector{Float64}`.
-
-# Arguments
-- `dpsim_matrix::dpsim_csr_matrix`: Input matrix
-- `rhs_vector::Vector{Float64}`: The right hand side of A * x = rhs
-- `samples::UInt=UInt(3)`: Number of executions (only lowest time counts)
-
-# Returns
-- `BenchmarkResult`: The benchmark result containing
-    - `decomp_elapses::Float64`: Duration of lu-decomposition
-    - `solve_elapses::Float64`: Duration of solving
-
-# Example
-```julia
-using Benchmark
-using Generator
-
-# gerator matrix
-settings = Generator.Settings(
-    dimension=300,
-    density=0.01
-)
-matrix = Generator.generate_matrix(settings)
-rhs_vector = Generator.generate_rhs_vector(matrix)
-result =  Benchmark.benchmark(matrix, rhs_vector) 
-```
 """
-function benchmark(dpsim_matrix::dpsim_csr_matrix, rhs_vector::Vector{Float64}; samples::UInt=UInt(3))
-    # Save Environment
-    saved_debug_env = get(ENV, "JULIA_DEBUG", "") # Use default value if not found
-    ENV["JULIA_DEBUG"] = "" # Disable debug information during execution
-
-    GC.enable(false)
-    system_matrix_ptr = pointer_from_objref(dpsim_matrix)
-    decomp_elapses = @belapsed decomp(Base.unsafe_convert(Ptr{dpsim_csr_matrix}, $system_matrix_ptr)) evals = samples
-
-    solve_elapses = @belapsed solve(Base.unsafe_convert(Ptr{Cdouble}, $rhs_vector),
-        Base.unsafe_convert(Ptr{Cdouble},
-            zeros(Float64, length($rhs_vector)))) evals = samples
-    GC.enable(true)
-
-    # Restore the previous env
-    ENV["JULIA_DEBUG"] = saved_debug_env
-
-    BenchmarkResult(
-        decomp_elapses,
-        solve_elapses
+function save_csv(path::AbstractString, metrics::Dict, strategy::Dict, matrix_path::String)
+    data_frame = DataFrame(
+        metrics=[metrics],
+        strategy=[strategy],
+        matrix_path=[matrix_path]
     )
-end
 
-"""
-    function benchmark(csr::SparseMatrixCSR, rhs_vector::Vector{Float64}; samples::UInt=UInt(3))
+    # Create folder for csv
+    mkpath(dirname("$path"))
 
-Wrapper for `function benchmark(dpsim_matrix::dpsim_csr_matrix, rhs_vector::Vector{Float64}; samples::UInt=UInt(3))`
-"""
-function benchmark(csr::SparseMatrixCSR, rhs_vector::Vector{Float64}; samples::UInt=UInt(3))
-    dpsim_matrix = Utils.csr_to_dpsim(csr)
-    benchmark(dpsim_matrix, rhs_vector; samples=samples)
-end
-
-"""
-    function benchmark(matrix::Matrix, rhs_vector::Vector{Float64}; samples::UInt=UInt(3))
-
-Wrapper for `function benchmark(dpsim_matrix::dpsim_csr_matrix, rhs_vector::Vector{Float64}; samples::UInt=UInt(3))`
-"""
-function benchmark(matrix::Matrix, rhs_vector::Vector{Float64}; samples::UInt=UInt(3))
-    csr = Utils.to_zerobased_csr(matrix)
-    benchmark(csr, rhs_vector; samples=samples)
-end
-
-"""
-    function benchmark(matrix_path::AbstractString, rhs_path::AbstractString; samples::UInt=UInt(3))
-
-Wrapper for `function benchmark(dpsim_matrix::dpsim_csr_matrix, rhs_vector::Vector{Float64}; samples::UInt=UInt(3))`
-"""
-function benchmark(matrix_path::AbstractString, rhs_path::AbstractString; samples::UInt=UInt(3))
-    dpsim_matrix = Utils.read_input(Utils.ArrayPath(matrix_path))
-    rhs_vector = Utils.read_input(Utils.VectorPath(rhs_path))
-    benchmark(dpsim_matrix, rhs_vector; samples=samples)
+    append = isfile("$path") # with append no header is written
+    CSV.write("$path", data_frame; append=append)
 end
 
 end
